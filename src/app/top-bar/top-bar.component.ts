0f22863645520e0e67b0eb6e5bc8de7cf59f8b7c
hirunppal <hirunppal@gmail.com>
2022-07-27 00:33:49 +0700

    ngif-ngfor

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -10,11 +10,23 @@ export class TopBarComponent implements OnInit {
   constructor() {}
   public isDisable: boolean = false;
   public handleOnclick() {
-    this.isDisable = !this.isDisable;
-    console.log(this.isDisable);
+    // this.isDisable = !this.isDisable;
+
+    // console.log(this.isDisable);
+    if (this.isAnyBlock.length < 10) {
+      const newArr = this.isAnyBlock.push(1);
+      // return (this.isAnyBlock = [newArr]);
+    } else {
+      this.isAnyBlock = [];
+    }
+    console.log(this.isAnyBlock);
   }
 
   public myInput: string = '';
+  public isAnyBlock: Array<any> = [];
+  isArremp() {
+    return this.isAnyBlock.length > 0 ? true : false;
+  }
   public handleOnInput(event: any) {
     console.log(event.target.value);
     this.myInput = event.target.value;
